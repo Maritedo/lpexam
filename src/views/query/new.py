9420ea7eb52f045120dc49a5b4cbb5f9767c73f9
Maritedo <62166314+Maritedo@users.noreply.github.com>
2023-12-12 02:08:32 +0800

    auto

--- src/views/query/new.py
+++ src/views/query/new.py
@@ -1,35 +1,36 @@
 ls = [
-    466,
-    467,
-    468,
-    469,
-    470,
-    471,
-    472,
-    473,
-    474,
-    475,
-    476,
-    477,
-    478,
-    479,
-    480,
-    481,
-    482,
-    505,
+    {"id": 513, "bank_name": "射箭23-12-11"},
+    {"id": 505, "bank_name": "飞盘2023-11-13"},
+    {"id": 482, "bank_name": "足球-2023.5"},
+    {"id": 481, "bank_name": "羽毛球20230510"},
+    {"id": 480, "bank_name": "瑜伽2023.5"},
+    {"id": 479, "bank_name": "游泳-2023.5"},
+    {"id": 478, "bank_name": "形体艺术2023.5"},
+    {"id": 477, "bank_name": "武术2023.5"},
+    {"id": 476, "bank_name": "网球20230510"},
+    {"id": 475, "bank_name": "体育舞蹈2023.5"},
+    {"id": 474, "bank_name": "跆拳道题库2023.5"},
+    {"id": 473, "bank_name": "散打2023,5"},
+    {"id": 472, "bank_name": "柔力球20230518"},
+    {"id": 471, "bank_name": "气排球23-05-19"},
+    {"id": 470, "bank_name": "普拉提2023.5"},
+    {"id": 469, "bank_name": "乒乓球20230510"},
+    {"id": 468, "bank_name": "排球23 05-19"},
+    {"id": 467, "bank_name": "篮球题库2023.5"},
+    {"id": 466, "bank_name": "拉丁舞20230510"},
+    {"id": 465, "bank_name": "健美操2023.5"},
 ]
 
-for i in ls:
-    with open(f"{i}.vue", "w", encoding="utf-8") as fw:
+with open(f"new.txt", "w", encoding="utf-8") as fw:
+    for i in ls:
         fw.write(
-            f'<template>\n\
-    <TemplateMain :quizs="exam"></TemplateMain>\n\
-</template>\n\
-\n\
-<script setup>\n\
-import TemplateMain from \'@/views/query/TemplateMain.vue\'\n\
-import {{ exam }} from "@/views/query/db/{i}"\n\
-</script>\n\
-\n\
-<style lang="scss" scoped></style>'
+            f"\n\
+      {{\n\
+        path: '{i['id']}',\n\
+        name: '{i['id']}',\n\
+        component: () => import('@/views/query/{i['id']}.vue'),\n\
+        meta: {{\n\
+          title: '{i['bank_name']}'\n\
+      }}\n\
+      }},"
         )
