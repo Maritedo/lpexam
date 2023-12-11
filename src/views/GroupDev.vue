0353de6bc1d9bb356d33d4aab2ceb8f0a5958375
Maritedo <62166314+Maritedo@users.noreply.github.com>
2023-12-11 22:16:19 +0800

    Initial commit

---
+++ src/views/GroupDev.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        这里是“开发”栏目，但现在什么也没有
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/GroupTool.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        这里是“工具”栏目，但现在什么也没有
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
